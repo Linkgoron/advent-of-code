7d1babeefc46c8115bce81b201d650e8b6ac63c8
linkgoron <linkgoron@gmail.com>
2018-12-04 22:40:15 +0200

    day1 done

---
+++ day1/ex2.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+fs.readFile('./ex1.input', (err, data) => {
+    if (err) throw new Error("data :(");
+
+    const set = new Set();
+    set.add(0);
+    const actualInput = data.toString();
+    const numbers = actualInput.split('\n').map(x => parseInt(x));
+    let acc = 0;
+    while (true) {        
+        for (const number of numbers) {
+            acc = acc + number;
+            if (set.has(acc)) {
+                console.log('found double!', acc);
+                return;
+            }
+
+            set.add(acc);
+        }
+    }
+}, 0);
+
+
