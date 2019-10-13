00d56acf36fad94e0e407234f129fd339e63a0a3
linkgoron <linkgoron@gmail.com>
2019-10-13 17:40:11 +0300

    day 22 ex2

--- 2018/day22/ex2.js
+++ 2018/day22/ex2.js
@@ -10,16 +10,152 @@ fs.readFile('./ex.input', (err, rawData) => {
         geological: {}
     }
 
-    let sum = 0;
-    for (let i = 0; i <= target.x; i++) {
-        for (let j = 0; j <= target.y; j++) {
-            sum += riskLevel(i, j, target, depth, memory);
+    // some limit to the graph size.
+    const extra = (target.x + target.y) / 2.2;
+    const map = new Map();
+    for (let i = 0; i <= Math.min(target.x + extra, depth); i++) {
+        for (let j = 0; j <= Math.min(target.y + extra, depth); j++) {
+            map.set(coordinates(i, j), {
+                x: i,
+                y: j,
+                type: pointType(i, j, target, depth, memory)
+            });
         }
     }
-    console.log(sum);
+
+    const graph = new Map();
+    for (let i = 0; i <= Math.min(target.x + extra, depth); i++) {
+        for (let j = 0; j <= Math.min(target.y + extra, depth); j++) {
+            const { type } = map.get(coordinates(i, j));
+            const curPossibleEquip = typeToOptions(type);
+            for (const curEquip of curPossibleEquip) {
+                const edges = [{ i: i - 1, j }, { i: i + 1, j }, { i, j: j - 1 }, { i, j: j + 1 }]
+                    .map(({ i, j }) => map.get(coordinates(i, j)))
+                    .filter(Boolean)
+                    .map(node => {
+                        const possibleEquipment = typeToOptions(node.type);
+                        const { x, y } = node;
+                        if (x === target.x && y === target.y) {
+                            if (type === 'wet') {
+                                return {
+                                    weight: curEquip === 'neither' ? (1 + 7) : 1 + 7 + 7,
+                                    equipment: 'torch',
+                                    x, y,
+                                    get name() { return `${coordinates(x, y)},${this.equipment}` }
+                                };
+                            }
+
+                            if (type === 'rocky') {
+                                return {
+                                    weight: curEquip === 'torch' ? 1 : 1 + 7,
+                                    equipment: 'torch',
+                                    x, y,
+                                    get name() { return `${coordinates(x, y)},${this.equipment}` }
+                                };
+                            }
+                            if (type === 'narrow') {
+                                return {
+                                    weight: curEquip === 'torch' ? 1 : 1 + 7,
+                                    equipment: 'torch',
+                                    x, y,
+                                    get name() { return `${coordinates(x, y)},${this.equipment}` }
+                                };
+                            }
+                            throw new Error('bad type');
+                        }
+
+                        if (possibleEquipment.some(e => curEquip === e)) {
+                            return {
+                                weight: 1,
+                                equipment: curEquip,
+                                x, y,
+                                get name() { return `${coordinates(this.x, this.y)},${this.equipment}` }
+                            }
+                        }
+
+                        return possibleEquipment.filter(posib => curPossibleEquip.some(e => e === posib)).map(equip => ({
+                            weight: 1 + 7,
+                            equipment: equip,
+                            x, y,
+                            get name() { return `${coordinates(this.x, this.y)},${this.equipment}` }
+                        }));
+                    }).flat();
+
+                graph.set(coordinates(i, j) + `,${curEquip}`, {
+                    x: i, y: j,
+                    edges
+                });
+            }
+        }
+    }
+    map.clear();
+    const toVisit = [{ name: `0,0,torch`, weight: 0 }];
+    const closest = () => toVisit.reduce((prev, cur, i) => {
+        if (prev === undefined) return { item: cur, i };
+        return cur.weight < prev.item.weight ? { item: cur, i } : prev;
+    }, undefined);
+    const willVisit = (name) => toVisit.findIndex(x => x.name === name);
+    const visited = new Set();
+
+    while (toVisit.length > 0) {
+        const { item: { name, weight, prev }, i } = closest();
+        const graphNode = graph.get(name);
+        const { x, y, edges } = graphNode;
+        graphNode.prev = prev;
+        visited.add(name);
+        toVisit.splice(i, 1);
+
+        if (x === target.x && y === target.y) {
+            console.log('the path:')
+            let path = prev;
+            console.log(name);
+            console.log(path);
+            while (path) {
+                const prevNode = graph.get(path)
+                console.log(prevNode.prev);
+                path = prevNode.prev;
+            }
+            return
+        }
+
+        for (const edge of edges) {
+            if (visited.has(edge.name)) {
+                continue;
+            }
+
+            const currentIndex = willVisit(edge.name);
+            if (currentIndex === -1) {
+                toVisit.push({ name: edge.name, weight: edge.weight + weight, prev: name });
+                continue;
+            }
+
+            const newWeight = edge.weight + weight;
+            const visitValue = toVisit[currentIndex];
+            if (visitValue.weight > newWeight) {
+                visitValue.weight = newWeight;
+                visitValue.prev = name;
+            }
+        }
+    }
+
+    console.log('done', graph.get());
+
 });
 
+function typeToOptions(type) {
+    if (type === 'rocky') {
+        return ['climbing', 'torch'];
+    }
+    if (type === 'wet') {
+        return ['climbing', 'neither'];
+    }
+    if (type === 'narrow') {
+        return ['torch', 'neither'];
+    }
+    throw new Error('bad type 2');
+}
 
+function coordinates(i, j) { return `${i},${j}` };
 
 function geologicalIndex(x, y, target, depth, memory) {
     if (x === 0 && y === 0) return 0;
@@ -51,6 +187,7 @@ function pointType(x, y, target, depth, memory) {
     if (erosion % 3 === 0) return 'rocky';
     if (erosion % 3 === 1) return 'wet';
     if (erosion % 3 === 2) return 'narrow';
+    throw new Error('bad erosion');
 }
 
 function riskLevel(x, y, target, depth, memory) {
@@ -58,4 +195,5 @@ function riskLevel(x, y, target, depth, memory) {
     if (type === 'rocky') return 0;
     if (type === 'wet') return 1;
     if (type === 'narrow') return 2;
+    throw new Error('bad type 3');
 }
