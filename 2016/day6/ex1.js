3b7252668b95e42a320febba6fd79c15327253ee
linkgoron <linkgoron@gmail.com>
2019-10-17 22:53:21 +0300

    day 6

---
+++ 2016/day6/ex1.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+fs.readFile('./ex.input', (err, data) => {
+    if (err) throw new Error("data :(");
+    const words = data.toString().split('\r\n').map(x => x.trim());
+    const rowLength = words[0].length;
+    let message = '';
+    for (let i = 0; i < rowLength; i++) {
+        const occurences = words.map(x => x[i]).reduce((acc, cur) => {
+            acc[cur] = (acc[cur] || 0) + 1;
+            return acc;
+        }, {});
+        message += Object.entries(occurences).sort(([a, b], [c, d])=> d - b)[0][0];
+    }
+    console.log(message);
+});
