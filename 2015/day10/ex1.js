bf1ba7acddb3f245d9d50b57ec49d5fe3bab0759
linkgoron <linkgoron@gmail.com>
2019-10-21 14:31:01 +0300

    day 10 2015

---
+++ 2015/day10/ex1.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+fs.readFile('./ex.input', (err, data) => {
+    if (err) throw new Error("data :(");
+    const input = data.toString();
+    let solution = input;
+    for (let iter = 0; iter < 40; iter++) {
+        let count = 0;
+        let res = "";
+        for (let letter = 0; letter < solution.length; letter++) {
+            if (solution[letter] !== solution[letter + 1]) {
+                res += (count + 1).toString() + solution[letter];
+                count = 0;
+                continue;
+            }
+            count++
+        }
+        solution = res;
+    }
+    console.log(solution.length)
+
+});
