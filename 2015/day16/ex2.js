f4aef2f1f9a79833e4d3fd94287f6296ba6e3360
linkgoron <linkgoron@gmail.com>
2019-10-21 18:11:32 +0300

    day 16 2015

---
+++ 2015/day16/ex2.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+const correct = {
+    children: 3,
+    cats: 7,
+    samoyeds: 2,
+    pomeranians: 3,
+    akitas: 0,
+    vizslas: 0,
+    goldfish: 5,
+    trees: 3,
+    cars: 2,
+    perfumes: 1
+}
+
+const props = Object.keys(correct);
+
+fs.readFile('./ex.input', (err, data) => {
+    if (err) throw new Error("data :(");
+    const aunts = data.toString().split('\r\n').map(x => x.trim()).map((x, i) => {
+        const start = x.indexOf(':') + 1;
+        const almostAunt = x.substring(start).replace(/[^ ]+:/g, x => `\"${x.replace(':', '')}\":`);
+        const aunt = JSON.parse(`{${almostAunt}}`);
+        aunt.number = i + 1;
+        return aunt;
+    });
+    const matching = aunts.filter(aunt => {
+        return props.every(x => {
+            if (x === 'cats' || x === 'trees') {
+                return aunt[x] > correct[x] || typeof aunt[x] === 'undefined';
+            }
+            if (x === 'pomeranians' || x === 'goldfish') {
+                return aunt[x] < correct[x] || typeof aunt[x] === 'undefined';
+            }
+            return aunt[x] === correct[x] || typeof aunt[x] === 'undefined';
+        })
+    })
+    console.log(matching);
+});
