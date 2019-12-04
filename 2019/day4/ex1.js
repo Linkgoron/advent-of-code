2b0b7e4727da5964df9ab16b8f0e84189aee0327
linkgoron <linkgoron@gmail.com>
2019-12-05 01:00:29 +0200

    day4

---
+++ 2019/day4/ex1.js
@@ -0,0 +1,26 @@
+
+require('fs').readFile('./ex1.input', (err, data) => {
+    var [min, max] = data.toString().split('-').map(x => parseInt(x));
+
+    var matches = 0;
+    for (let num = min; num <= max; num++) {
+        if (isOK(num.toString())) {
+            matches++;
+        }
+    }
+    console.log(matches);
+});
+
+function isOK(arr) {
+    let hasDouble = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] === arr[i + 1]) {
+            hasDouble = true;
+        }
+        
+        if (parseInt(arr[i]) > parseInt(arr[i + 1])) {
+            return false;
+        }
+    }
+    return hasDouble;
+}
