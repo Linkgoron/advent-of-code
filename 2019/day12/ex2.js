8830b12345ad5a3ad366e12edd1b62d1d9027f8f
linkgoron <linkgoron@gmail.com>
2019-12-13 00:29:43 +0200

    cleanup

--- 2019/day12/ex2.js
+++ 2019/day12/ex2.js
@@ -2,7 +2,6 @@ require('fs').readFile('./ex2.input', (err, data) => {
     const moons = data.toString().trim()
         .split('\n').map(x => x.trim().substring(1, x.length - 1).split(',').map(x => x.trim().split('=')))
         .map((row, i) => ({
-            id: i,
             position: {
                 x: parseInt(row[0][1]),
                 y: parseInt(row[1][1]),
@@ -11,59 +10,37 @@ require('fs').readFile('./ex2.input', (err, data) => {
             velocity: { x: 0, y: 0, z: 0 }
         }));
 
-    let initialXs = moons.map(x => x.position.x).toString();
-    let initialYs = moons.map(x => x.position.y).toString();
-    let initialZs = moons.map(x => x.position.z).toString();
+    const initial = {
+        x: moons.map(x => x.position.x).toString(),
+        y: moons.map(y => y.position.y).toString(),
+        z: moons.map(z => z.position.z).toString(),
+    }
+
+    const repetition = { x: false, y: false, z: false };
     let state = moons;
-
-    let hasX = false;
-    let hasY = false;
-    let hasZ = false;
-
-    let factors = [];
-
-    for (let i = 0; i < 1000000; i++) {
-        const changing = state.map(moon => ({
-            moon,
-            changes: state.map(otherMoon => velocityChange(moon.position, otherMoon.position))
-        }));
-        state = changing.map(x => {
-            const velocity = computeTotalVelocity(x.moon.velocity, x.changes);
-            const position = addTuples(x.moon.position, velocity);
+    for (let i = 0; !repetition.x || !repetition.y || !repetition.z; i++) {
+        state = state.map(({ position, velocity }) => {
+            const vel = state.reduce((a, otherMoon) => add(a, velocityChange(position, otherMoon.position)), velocity);
             return {
-                id: x.moon.id,
-                position,
-                velocity
+                position: add(position, vel),
+                velocity: vel
             }
         });
 
-        var currentX = state.map(x => x.position.x).toString();
-        var currentY = state.map(x => x.position.y).toString();
-        var currentZ = state.map(x => x.position.z).toString();
-
-        if (!hasX && currentX == initialXs && state.every(m => m.velocity.x === 0)) {
-            factors.push(findPrimeFactors(i + 1))
-            hasX = true;
-        }
-        if (!hasY && currentY == initialYs && state.every(m => m.velocity.y === 0)) {
-            factors.push(findPrimeFactors(i + 1))
-            hasY = true;
-        }
-        if (!hasZ && currentZ == initialZs && state.every(m => m.velocity.z === 0)) {
-            factors.push(findPrimeFactors(i + 1))
-            hasZ = true;
-        }
-        if (hasX && hasY && hasZ) {
-            break;
+        for (const axis of ['x', 'y', 'z']) {
+            const currentAxisValue = state.map(x => x.position[axis]).toString();
+            if (!repetition[axis] && currentAxisValue == initial[axis] && state.every(m => m.velocity[axis] === 0)) {
+                repetition[axis] = findPrimeFactors(i + 1);
+            }
         }
     }
 
     // compute LCM
-    const allNums = new Set(factors.map(x => Object.keys(x)).reduce((all, me) => all.concat(me), []));
-
-    const LCM = [...allNums].map(key => factors.reduce((a, b) => {
-        if (key in b) {
-            let compute = Number(key) ** b[key]
+    const values = Object.values(repetition);
+    const allNums = new Set(values.map(x => Object.keys(x)).reduce((all, me) => all.concat(me), []));
+    const LCM = [...allNums].map(primeFactor => values.reduce((a, factors) => {
+        if (primeFactor in factors) {
+            const compute = Number(primeFactor) ** factors[primeFactor];
             return compute > a ? compute : a;
         }
         return a;
@@ -79,25 +56,20 @@ require('fs').readFile('./ex2.input', (err, data) => {
         return { x: diff(pos1.x, pos2.x), y: diff(pos1.y, pos2.y), z: diff(pos1.z, pos2.z) };
     }
 
-    function addTuples(tup1, tup2) {
+    function add(tup1, tup2) {
         return { x: tup1.x + tup2.x, y: tup1.y + tup2.y, z: tup1.z + tup2.z };
-    }
-
-    function computeTotalVelocity(velocity, changes) {
-        return changes.reduce((prev, cur) => addTuples(prev, cur), { ...velocity });
     }
 });
 
 function findPrimeFactors(num) {
-
-    var primeFactors = [];
+    const primeFactors = [];
     while (num % 2 === 0) {
         primeFactors.push(2);
         num = num / 2;
     }
 
-    var sqrtNum = Math.sqrt(num);
-    for (var i = 3; i <= sqrtNum; i++) {
+    const sqrtNum = Math.sqrt(num);
+    for (let i = 3; i <= sqrtNum; i++) {
         while (num % i === 0) {
             primeFactors.push(i);
             num = num / i;
