eb913d3bab1bf84343a162c7fbf0e96a65c8ebf1
linkgoron <linkgoron@gmail.com>
2019-12-19 20:54:18 +0200

    2019 day 19

---
+++ 2019/day19/ex1.js
@@ -0,0 +1,122 @@
+require('fs').readFile('./ex.input', (err, data) => {
+    const startingState = data.toString().trim().split(',').map(x => parseInt(x));
+    let taken = 0;
+    for (let x = 0; x < 50; x++) {
+        for (let y = 0; y < 50; y++) {
+            const robot = new RobotModule(x, y);
+            runProgram(startingState, robot, robot);
+            taken += robot.output;
+        }
+    }
+    console.log(taken);
+});
+
+function runProgram(startingState, input, output) {
+    const state = [...startingState];
+    let relative = 0;
+    for (let inst = 0; inst < state.length; inst++) {
+        const totalCommand = state[inst++];
+        const command = totalCommand % 100;
+        if (![1, 2, 3, 4, 5, 6, 7, 8, 9, 99].includes(command)) {
+            console.error('bad op-code', inst - 1, totalCommand, command);
+            throw new Error('bad op code');
+        }
+
+        if (command === 99) {
+            return;
+        }
+        const modeString = [...Math.floor(totalCommand / 100).toString()].reverse()
+        const modes = [parseInt(modeString[0] || '0'), parseInt(modeString[1] || '0'), parseInt(modeString[2] || '0')]
+
+        function fetchValue(mode, value, memory) {
+            return mode === 1 ? value :
+                mode === 0 ? (memory[value] || 0) :
+                    (memory[relative + value] || 0);
+        }
+
+        function getWritePosition(mode, value, memory) {
+            return mode === 0 ? memory[value] : (memory[inst] + relative);
+        }
+
+        switch (command) {
+            case 1: {
+                const left = fetchValue(modes[0], state[inst++], state);
+                const right = fetchValue(modes[1], state[inst++], state);
+                const writePos = getWritePosition(modes[2], inst, state);
+                state[writePos] = left + right;
+                continue
+            }
+            case 2: {
+                const left = fetchValue(modes[0], state[inst++], state);
+                const right = fetchValue(modes[1], state[inst++], state);
+                const writePos = getWritePosition(modes[2], inst, state);
+                state[writePos] = left * right;
+                continue
+            }
+            case 3: {
+                const writePos = getWritePosition(modes[0], inst, state);
+                state[writePos] = input.read();
+                continue;
+            }
+            case 4: {
+                const out = fetchValue(modes[0], state[inst], state);
+                output.write(out);
+                continue;
+            }
+            case 5: {
+                const first = fetchValue(modes[0], state[inst++], state);
+                if (first !== 0) {
+                    inst = fetchValue(modes[1], state[inst], state) - 1;
+                }
+                continue;
+            }
+            case 6: {
+                const first = fetchValue(modes[0], state[inst++], state);
+                if (first === 0) {
+                    inst = fetchValue(modes[1], state[inst], state) - 1;
+                }
+                continue;
+            }
+            case 7: {
+                const first = fetchValue(modes[0], state[inst++], state);
+                const second = fetchValue(modes[1], state[inst++], state);
+                const writePos = getWritePosition(modes[2], inst, state);
+                state[writePos] = first < second ? 1 : 0;
+                continue;
+            }
+            case 8: {
+                const first = fetchValue(modes[0], state[inst++], state);
+                const second = fetchValue(modes[1], state[inst++], state);
+                const writePos = getWritePosition(modes[2], inst, state);
+                state[writePos] = first === second ? 1 : 0;
+                continue;
+            }
+            case 9: {
+                const value = fetchValue(modes[0], state[inst], state);
+                relative += value;
+                continue;
+            }
+        }
+    }
+}
+class RobotModule {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.inputState = 0;
+        this.output = undefined;
+    }
+
+    write(value) {
+        this.output = value;
+    }
+
+    read() {
+        if (this.inputState++ === 0) {
+            return this.x;
+        } else {
+            return this.y;
+        }
+
+    }
+}
