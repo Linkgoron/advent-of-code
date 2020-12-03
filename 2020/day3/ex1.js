0561e2ac1f9abfa771bfc174f2705a604ecb7f8d
linkgoron <linkgoron@gmail.com>
2020-12-03 19:41:33 +0200

    remove vars

--- 2020/day3/ex1.js
+++ 2020/day3/ex1.js
@@ -3,8 +3,6 @@ fs.promises.readFile('./ex.input').then(data => {
     const map = data.toString().trim().split('\n').map(x => x.trim().split(''));
     let width = map[0].length;
     let height = map.length;
-    let i = 0;
-    let j = 0;
     const iJump = 3;
     const jJump = 1;
     let total = 0;
