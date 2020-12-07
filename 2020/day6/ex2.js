e8961eb662124afee595b589e1694168a960f541
Nitzan Uziely <nitzan@testim.io>
2020-12-07 11:26:54 +0200

    day 6 multiplatform

--- 2020/day6/ex2.js
+++ 2020/day6/ex2.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 fs.promises.readFile('./ex.input').then(data => {
-    const rows = data.toString().trim().split('\n\n').map(x => x.split('\n'));
+    const rows = data.toString().trim().split(/\r?\n\r?\n/gm).map(x => x.split(/\r?\n/gm));
     const answers = rows.map(group => {
         const min = group.reduce((acc, x) => x.length < acc.length ? x : acc, group[0]);
         const intersection = new Set(min);
