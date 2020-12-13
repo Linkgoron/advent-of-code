e3d9eb8fdd48d95e3429322c2203fffcd175220b
Nitzan Uziely <nitzan@testim.io>
2020-12-13 15:52:26 +0200

    cleanup

--- 2020/day13/ex1.js
+++ 2020/day13/ex1.js
@@ -3,7 +3,7 @@ fs.promises.readFile('./ex.input').then(raw => {
     const [rawWait, busses] = raw.toString().trim().split(/\r?\n/gm);
     const stuff = busses.split(',').filter(x => x !== 'x').map(Number);
     const wait = Number(rawWait);
-    const waits = stuff.map(x => ({ x, after: x - wait % x, waiting: wait + (x - wait % x), val: (x - wait % x) * x }));
-    const min = waits.reduce((acc,cur)=> cur.after < acc.after ? cur : acc,waits[0]);
-    console.log(min);
+    const waits = stuff.map(x => ({ after: x - wait % x, val: (x - wait % x) * x }));
+    const min = waits.reduce((acc, cur) => cur.after < acc.after ? cur : acc, waits[0]);
+    console.log(min.val);
 });
