231067e8a5a2cca5e70d904dc168ac505e9d9b1a
Nitzan Uziely <nitzan@testim.io>
2020-12-15 12:07:12 +0200

    day 15

---
+++ 2020/day15/ex2.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+fs.promises.readFile('./ex.input').then(raw => {
+    const numbers = raw.toString().trim().split(',').map(Number);
+    console.log(numbers);
+    const mem = new Map();
+    let prev = undefined;
+    const turnLimit = 30000000;
+    for (let turn = 0; turn < turnLimit; turn++) {       
+        let said = undefined;
+        if (turn < numbers.length) {
+            said = numbers[turn];
+            mem.set(said, turn);
+        } else {
+            if (mem.has(prev)) {
+                said = turn - mem.get(prev) - 1;
+            } else {
+                said = 0;
+            }
+            mem.set(prev, turn - 1);
+        }
+        prev = said;
+    }
+    console.log(prev);
+});
